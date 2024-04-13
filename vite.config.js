cd4a6b4274bb9efba66423e20127309f9cbe7949
Dwight LaGrone <dwightlagrone@yahoo.com>
2024-04-12 22:52:40 -0500

    Commit - 041224 v12

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+export default defineConfig(({ command }) => {
+    if (command === 'serve') {
+        return {
+            plugins: [vue()]
+        }
+    } else {
+        return {
+            plugins: [vue()],
+            build: {
+                target: ['chrome60', 'firefox60', 'safari11', 'edge18'],
+            }
+        }
+    }
 })
