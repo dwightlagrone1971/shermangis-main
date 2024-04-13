9f79b68addd0b3592f0d18070ef1de5d0d9188ba
Dwight LaGrone <dwightlagrone@yahoo.com>
2024-04-12 23:50:33 -0500

    Commit - 041424 v14

--- src/views/PDFMaps.vue
+++ src/views/PDFMaps.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="font-serif m-auto lg:w-11/12 2xl:w-8/12">
-    <h1 class="font-serif m-auto text-5xl mt-10 mb-12 font-bold 2xl:mt-18 2xl:mb-20" style="color: #4a4a4a">Maps</h1>
-    <div class="m-auto grid grid-cols-1 lg:grid-cols-4 lg:w-11/12 2xl:w-12/12">
-      <button
+  <div>
+    <h1 class="font-serif text-5xl mt-12 mb-12 2xl:mt-18 2xl:mb-8 font-bold" style="color: #4a4a4a">GIS Downloads</h1>
+    <div class="m-auto pb-12 pt-10 grid grid-cols-1 lg:grid-cols-3 lg:w-9/12 2xl:w-6/12">
+      <div
         v-for="item in items"
         :key="item"
         style="background-color: #4a4a4a;"
-        class="m-auto mb-6 pb-10 w-56 2xl:w-60 h-12 inline-block transition transform hover:-translate-y-3 motion-reduce:transition-none motion-reduce:transform-none">
+        class="font-serif m-auto mb-6 pb-10 w-60 2xl:w-60 h-12 inline-block transition transform hover:-translate-y-3 motion-reduce:transition-none motion-reduce:transform-none">
         <a :href="item.url" :title="item.name" target="_blank">
           <div
-            class="font-serif text-md p-3"
+            class="font-serif text-md p-3 2xl:mb-6"
             style="color: #f8f8f8;">
             {{ item.name }}
           </div>
         </a>
-      </button>
+      </div>
     </div>
   </div>
 </template>
@@ -26,7 +26,7 @@ import { computed, ref } from 'vue'
 export default {
   setup() {
     const store = useStore();
-    const items = computed(() => store.getters.getItems('pdfMaps'))
+    const items = computed(() => store.getters.getItems('pdfmaps'))
 
 
     return { items, store }
