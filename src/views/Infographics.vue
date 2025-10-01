a55f4b52692494621bd5daced311a9d5bc0236ec
Dwight LaGrone <dwightlagrone@yahoo.com>
2025-10-01 15:00:52 -0500

    Commit - 10012025 v2

--- src/views/Infographics.vue
+++ src/views/Infographics.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="bg-white border-2 border-gray-100">
-    <div class="text-red-900 -bottom-11font-serif m-auto w-10/12 my-10 lg:w-9/12 2xl:w-6/12">
-      <p class="text-red-900 font-serif m-auto mb-6 2xl:py-10 text-xl font-bold">
-        <span class="text-4xl text-red-900">Infographics </span>are a visual representation of information
- or data. The infographics below use American Community
- Survey (ACS), Census, Esri forecasts, and third-party retail data.</p>
-      </div>
-  <div class="m-auto mb-10 w-40 grid h-fit pb-4 grid-cols-1 2xl:h-auto 2xl:w-1/2 2xl:grid-cols-5 2xl:gap-10">
-    <button
-      v-for="item in items"
-        v-on:click="openPdf"
-        :key="item"
-    >
-        <a :href=item.url target= "_blank">
-          <img 
-          class="shadow-2xl h-auto transition transform hover:-translate-y-3 motion-reduce:transition-none motion-reduce:transform-none" 
-          :src=item.src alt="">
-        </a>       
-        <h3 class="font-bold mb-10 mt-2 text-red-900">{{ item.name }}</h3>
-    </button>
+      <div class="h-screen">
+        <div class="text-red-900 -bottom-11font-serif m-auto w-10/12 my-10 lg:w-9/12 2xl:w-6/12">
+      <p class="text-red-900 font-serif m-auto mb-6 2xl:py-10 text-lg font-bold">
+        <span class="text-4xl text-red-900">Infographics </span>are a visual representation of information or data. The infographics below use American Community Survey (ACS), Census, Esri forecasts, and third-party retail data.
+      </p>
     </div>
+        <div class="m-auto mb-10 w-40 grid h-fit pb-4 grid-cols-1 2xl:h-auto 2xl:w-1/2 2xl:grid-cols-5 2xl:gap-10">
+          <div
+            v-for="item in items"
+            :key="item"
+          >
+            <a :href=item.url target=”_blank >
+            <img 
+              class="shadow-2xl h-auto transition transform hover:-translate-y-3 motion-reduce:transition-none motion-reduce:transform-none" 
+              :src=item.src alt="">
+            </a>       
+            <h3 class="text-red-900 font-bold mb-10 mt-2">{{ item.name }}</h3>
+          </div>
+        </div>
+      </div>
   </div>
 </template>
 
